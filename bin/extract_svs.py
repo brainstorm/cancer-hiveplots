dea323425553378aad569cc842ebac04b6028df5
Roman Valls Guimera <brainstorm@nopcode.org>
2016-02-08 09:52:43 +0100

    ICGC tweaks on extract_svs.py to account for SVCLASS and ICGCs structure. Ping @ohofmann @guillermo-carrasco

--- bin/extract_svs.py
+++ bin/extract_svs.py
@@ -3,26 +3,26 @@
 """
 import csv
 import os
+import glob
 import subprocess
-from argparse import ArgumentParser
 
 import pysam
 
 def main():
-    samples = {"APGI": ["APGI1953_Tumor", "APGI1955_Tumor", "APGI2049_Tumor"]}
-    chroms = {"APGI": [str(x) for x in range(1, 23)]}
-    callers = ["manta"]
-
+    samples = {os.path.basename(k): ["TUMOR", "NORMAL"] for k in glob.glob("inputs/*.vcf.gz")}
+    chroms = [str(x) for x in range(1, 23)]
+    callers = ["BASS"]
     depths = [10, 25]
     approaches = ["full", "subset"]
-    for project, samples in samples.items():
+
+    for fname, samples in samples.items():
         for depth in depths:
             for approach in approaches:
                 if approach == "full":
                     cur_chroms = []
                 else:
-                    cur_chroms = chroms[project]
-                extract_svs(project, samples, depth, cur_chroms, callers)
+                    cur_chroms = chroms
+                extract_svs(fname, samples, depth, cur_chroms, callers)
 
 def extract_svs(project, samples, depth, chroms, callers):
     """Create CSV file of structural variants of interest, for Circos plots.
@@ -33,27 +33,21 @@ def extract_svs(project, samples, depth, chroms, callers):
         os.makedirs(out_dir)
     out_file = os.path.join(out_dir, "%s-svs-%s-dp%s.csv" % (project, ("subset" if chroms else "full"), depth))
     region_bed = "%s-regions.bed" % os.path.splitext(out_file)[0]
+
     with open(out_file, "w") as out_handle:
         with open(region_bed, "w") as region_out:
             writer = csv.writer(out_handle)
-            writer.writerow(["chrom1", "start1", "end1", "chrom2", "start2", "end2", "sample", "caller", "svtype"])
+            writer.writerow(["chrom1", "start1", "end1", "chrom2", "start2", "end2", "file", "sample", "caller", "svtype"])
             for sample in samples:
                 for ci, caller in enumerate(callers):
-                    sv_file = os.path.join("inputs", "%s-%s.vcf.gz" % (sample, caller))
-                    for p1, p2, svtype in parse_svs(sv_file, depth):
+                    sv_file = os.path.join("inputs", "%s" % project)
+                    for p1, p2, svtype in parse_svs(sv_file, sample, depth):
                         if len(chroms) == 0 or (p1[0] in chroms or p2[0] in chroms):
                             if p1[0] in allowed_chroms and p2[0] in allowed_chroms:
-                                writer.writerow(p1 + p2 + [sample, caller, svtype])
+                                writer.writerow(p1 + p2 + [project, sample, caller, svtype])
                                 if ci == 0:
                                     for (chrom, start, end) in [p1, p2]:
-                                        region_out.write("%s\t%s\t%s\t%s\n" % (chrom, start - 1, end, sample))
-    # filter_shared(out_file, region_bed)
-    # noshared_file = "%s-noshared%s" % os.path.splitext(out_file)
-    # with open(noshared_file, "w") as out_handle:
-    #     writer = csv.writer(out_handle)
-    #     writer.writerow(["chrom1", "start1", "end1", "chrom2", "start2", "end2", "sample", "caller", "svtype"])
-    #     for row in filter_shared(out_file, region_bed):
-    #         writer.writerow(row)
+                                        region_out.write("%s\t%s\t%s\t%s\t%s\n" % (chrom, start - 1, end, project, sample))
 
 def filter_shared(in_file, region_bed):
     cmd = "sort -k1,1 -k2,2n {region_bed} | bedtools merge -i - -d 10000 -c 4 -o count_distinct"
@@ -69,35 +63,43 @@ def filter_shared(in_file, region_bed):
     print shared, noshared
     return []
 
-def parse_svs(in_file, depth):
+def parse_svs(in_file, sample, depth):
     bnds = {}
     min_su_bnd = depth
     min_su_other = depth
     for rec in pysam.VariantFile(in_file):
         if passes(rec):
-            if rec.info["SVTYPE"] == "BND":
-                if rec.info.get("SU") is None or rec.info["SU"] > min_su_bnd:
-                    p = [rec.chrom.replace("chr", ""), rec.start, rec.start]
-                    if rec.id in bnds:
-                        p_o = bnds[rec.id]
-                        yield p_o, p, rec.info["SVTYPE"]
-                        del bnds[rec.id]
-                    else:
-                        mate_id = rec.info.get("MATEID")
-                        bnds[mate_id] = p
+            if rec.headers.sample == sample:
+                if rec.info["SVTYPE"] == "BND":
+                    if rec.info.get("SU") is None or rec.info["SU"] > min_su_bnd:
+                        p = [rec.chrom.replace("chr", ""), rec.start, rec.start]
+                        if rec.id in bnds:
+                            p_o = bnds[rec.id]
+                            # ICGC samples encode SVTYPE (all appear to be BND)
+                            # into SVCLASS in INFO (insertion, deletion)
+                            if rec.info["SVCLASS"] is not None:
+                                yield p_o, p, rec.info["SVCLASS"]
+                            else:
+                                yield p_o, p, rec.info["SVTYPE"]
+                            del bnds[rec.id]
+                        else:
+                            mate_id = rec.info.get("MATEID")
+                            bnds[mate_id] = p
+                else:
+                    if rec.info.get("SU") is None or rec.info["SU"] > min_su_other:
+                        p1 = [rec.chrom.replace("chr", ""), rec.start, rec.start]
+                        p2 = [rec.chrom.replace("chr", ""), rec.info["END"], rec.info["END"]]
+                        yield p1, p2, rec.info["SVTYPE"]
             else:
-                if rec.info.get("SU") is None or rec.info["SU"] > min_su_other:
-                    p1 = [rec.chrom.replace("chr", ""), rec.start, rec.start]
-                    p2 = [rec.chrom.replace("chr", ""), rec.info["END"], rec.info["END"]]
-                    yield p1, p2, rec.info["SVTYPE"]
-
+                # we don't want to mixup TUMOR/NORMAL records, do we? :-S
+                continue
 def passes(rec):
-    if rec.filter.keys() == 0 or rec.filter.keys()[0] == "PASS":
-        if not rec.samples[0].get("GT"):
-            return True
-        elif list(set(rec.samples[0].get("GT"))) != ["N"]:
-            return True
-    return False
+    #if rec.filter.keys() == 0 or rec.filter.keys()[0] == "PASS":
+    #    if not rec.samples[0].get("GT"):
+    #        return True
+    #    elif list(set(rec.samples[0].get("GT"))) != ["N"]:
+    #        return True
+    return True
 
 if __name__ == "__main__":
     main()
